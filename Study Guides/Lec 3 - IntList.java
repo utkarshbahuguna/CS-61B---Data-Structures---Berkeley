a31c5fa0ea7e4caac5b325b3ae490d8dfd424713
Utkarsh Bahuguna <utkarsh.bahuguna1@gmail.com>
2021-05-26 21:11:07 +0530

    Completed study guide exercises on Intlists and SLLists

---
+++ Study Guides/Lec 3 - IntList.java
@@ -0,0 +1,132 @@
+/* https://fa20.datastructur.es/materials/lectures/lec3/lec3.html
+   https://fa20.datastructur.es/materials/lectures/lec4/lec4
+   https://www.kartikkapur.com/documents/mt1.pdf#page=7
+ */
+
+class IntList {
+	public int first;
+	public IntList rest;
+
+	public IntList(int f, IntList r) {
+		first = f;
+		rest = r;
+	}
+
+	// Lec 3 - B Level
+	/** Return the size of the list using... recursion! */
+	public int size() {
+		if (this.rest == null) {
+            return 1;
+        } else {
+            return 1 + this.rest.size();
+        }
+	}
+
+	/** Return the size of the list using no recursion! */
+	public int iterativeSize() {
+		IntList p = this;
+        int size = 1;
+
+        while (p.rest != null) {
+            size += 1;
+            p = p.rest;
+        }
+        return size;
+	}
+
+	/** Returns the ith value in this list.*/
+	public int get(int i) {
+        if (i == 0) {
+            return first;
+        } else {
+            return rest.get(i - 1);
+        }
+	}
+
+     /** Returns an IntList identical to L, but with
+      * each element incremented by x. L is not allowed
+      * to change. */
+    public static IntList incrList(IntList L, int x) {
+        if (L.rest == null) {
+            L = new IntList(L.first + x, null);
+        } else {
+            L = new IntList(L.first + x, incrList(L.rest, x));
+        }
+        return L;
+    }
+
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. Not allowed to use
+      * the 'new' keyword. */
+    public static IntList dincrList(IntList L, int x) {
+        if (L.rest == null) {
+            L.first += x;
+        } else {
+            L.first += x;
+            L.rest = dincrList(L.rest, x);
+        }
+        return L;
+    }
+
+    /** Adds x to the end of IntList. */
+    public void addLast(int x) {
+        IntList p = this;
+        while (p.rest != null) {
+            p = p.rest;
+        }
+        p.rest = new IntList(x, null);
+    }
+
+    /** Adds x to the end of the list and adds squares of all existing list elements right after them. */
+    public void addLastSquare(int x) {
+        IntList p = this;
+        while (p.rest != null) {
+            p.rest = new IntList(p.first * p.first, p.rest);
+            p = p.rest.rest;
+        }
+        IntList addition = new IntList(x, null);
+        p.rest = new IntList(p.first * p.first, addition);
+    }
+
+    /** If two numbers in a row are the same, adds them together to form a larger node. */
+    public void addAdjacent() {
+        IntList p = this;
+        while (p.rest != null) {
+            if (p.first == p.rest.first) {
+                p.first = p.first + p.rest.first;
+                p.rest = p.rest.rest;
+            } else {
+                p = p.rest;
+            }
+        }
+    }
+
+    public void printList() {
+        for (int i = 0; i < this.size(); i++) {
+            System.out.print(this.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+
+	public static void main(String[] args) {
+		IntList L = new IntList(15, null);
+		L = new IntList(10, L);
+		L = new IntList(5, L);
+
+		System.out.println(L.iterativeSize());
+		System.out.println(L.size());
+		System.out.println(L.get(0));
+		System.out.println(L.get(1));
+        System.out.println(incrList(L, 3).get(1));
+		L.printList();
+        dincrList(L, 3);
+        L.printList();
+        L.addLast(18);
+        L.printList();
+        L.addAdjacent();
+        L.printList();
+        L.addLastSquare(7);
+        L.printList();
+	}
+} 
---
+++ Study Guides/Lec 4 - SLList.java
@@ -0,0 +1,95 @@
+// https://fa20.datastructur.es/materials/lectures/lec4/lec4
+
+/** An SLList is a list of integers, which hides the terrible truth
+   * of the nakedness within. */
+class SLList {	
+	private static class IntNode {
+		public int item;
+		public IntNode next;
+
+		public IntNode(int i, IntNode n) {
+			item = i;
+			next = n;
+		}
+	} 
+
+	/* The first item (if it exists) is at sentinel.next. */
+	private IntNode sentinel;
+	private int size;
+
+	public void deleteFirst () {
+		if (this.size > 0) {
+			sentinel.next = sentinel.next.next;
+			size = size - 1;
+		}
+	}
+
+	/** Creates an empty SLList. */
+	public SLList() {
+		sentinel = new IntNode(63, null);
+		size = 0;
+	}
+
+	public SLList(int x) {
+		sentinel = new IntNode(63, null);
+		sentinel.next = new IntNode(x, null);
+		size = 1;
+	}
+
+	/** Creates an SLList from an array of integers. */
+	public SLList(int[] arr) {
+		sentinel = new IntNode(63, null);
+		IntNode p = sentinel;
+		for (int x : arr){
+			p.next = new IntNode(x, null);
+			p = p.next;
+		}
+		size = arr.length;
+	}
+
+ 	/** Adds x to the front of the list. */
+ 	public void addFirst(int x) {
+ 		sentinel.next = new IntNode(x, sentinel.next);
+ 		size = size + 1;
+ 	}
+
+ 	/** Returns the first item in the list. */
+ 	public int getFirst() {
+ 		return sentinel.next.item;
+ 	}
+
+ 	/** Adds x to the end of the list. */
+ 	public void addLast(int x) {
+ 		size = size + 1; 		
+
+ 		IntNode p = sentinel;
+
+ 		/* Advance p to the end of the list. */
+ 		while (p.next != null) {
+ 			p = p.next;
+ 		}
+
+ 		p.next = new IntNode(x, null);
+ 	}
+ 	
+ 	/** Returns the size of the list. */
+ 	public int size() {
+ 		return size;
+ 	}
+
+	public static void main(String[] args) {
+ 		/* Creates a list of one integer, namely 10 */
+ 		SLList L = new SLList();
+ 		L.addLast(20);
+ 		L.addFirst(5);
+ 		System.out.println(L.getFirst());
+ 		L.deleteFirst();
+		System.out.println(L.getFirst());
+ 		System.out.println(L.size());
+
+ 		SLList Lst = new SLList(new int[]{100, 2, 3, 5, 10});
+		System.out.println(Lst.getFirst());
+		System.out.println(Lst.size());
+ 	}
+}
+
