f6f5bcbc621594b40a9c89e93aaf9e068818e68a
Utkarsh Bahuguna <utkarsh.bahuguna1@gmail.com>
2021-05-28 04:36:23 +0530

    pulled LectureCode from remote repo as a submodule

---
+++ Study Guides/lec05/Lec05.java
@@ -0,0 +1,7 @@
+// https://fa20.datastructur.es/materials/lectures/lec5/lec5
+
+// All objects in Java extend the Object class. So generic arrays can be formed as new Object[].
+// 2D arrays with different types can be formed using new Objects [][].
+
+public class Lec05 {
+}
