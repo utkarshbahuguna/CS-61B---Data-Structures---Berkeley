983c57ed77b07d9d38ac36cc669832e30e5df940
Utkarsh Bahuguna <utkarsh.bahuguna1@gmail.com>
2021-06-07 09:36:58 +0530

    Completed HW2 : Estimating the value of the percolation threshold via Monte Carlo simulation.

--- Homeworks/hw2/hw2/Percolation.java
+++ Homeworks/hw2/hw2/Percolation.java
@@ -1,7 +1,124 @@
+// https://fa20.datastructur.es/materials/hw/hw2/hw2
+
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Percolation {
+    private WeightedQuickUnionUF grid;
+    private HashMap sites;
+    private int N;
+    private int openSites;
+    boolean percolates;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) { throw new IllegalArgumentException("Grid N has to be positive."); }
+        this.N = N;
+        openSites = 0;
+        grid = new WeightedQuickUnionUF(N * N);
+        sites = new HashMap<Integer, Integer>();
+        percolates = false;
+    }
+
+    // Returns true if x can be part of a valid co-ordinate in grid, else false.
+    private boolean valid(int... x) {
+        for (int i : x) {
+            if (!(i >= 0 && i < N)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Returns the address of a cell if the grid was placed with rows N by N in a one-dimensional array.
+    private int linearAddress(int row, int col) {
+        return row * N + col;
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if(!valid(row, col)) { throw new IndexOutOfBoundsException(); }
+        if (isOpen(row, col)) { return; }
+
+        int self = linearAddress(row, col);
+
+        if (row == 0) { sites.put(self, 1);
+        } else if (row == N -1) { sites.put(self, 2);
+        } else { sites.put(self, 0); }
+
+        openSites += 1;
+        for (int i = -1; i <= 1; i = i+1) {
+            for (int j = -1; j <= 1; j = j+1) {
+                if (openNeighbor(row, col , i, j)) {
+                    int selfComponent = grid.find(self);
+                    int neighbor = linearAddress(row + i, col + j);
+                    int neighborComponent = grid.find(neighbor);
+
+                    grid.union(self, neighbor);
+
+                    int newValue;
+                    int selfValue = (int) sites.get(selfComponent);
+                    int neighborValue = (int) sites.get(neighborComponent);
+
+                    newValue = (selfValue == neighborValue) ? selfValue : Math.min(3, selfValue + neighborValue);
+
+                    sites.put(grid.find(self), newValue);
+
+                    if ((int) sites.get(grid.find(self)) == 3) {
+                        percolates = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // is the cell in row + i, col + j a valid open neighbor?
+    private boolean openNeighbor(int row, int col, int i, int j) {
+        return valid(row + i, col + j) && (i == 0 || j == 0)  && isOpen(row + i, col + j);
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if(!valid(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return sites.containsKey(linearAddress(row, col));
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if(!valid(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        int state = (int) sites.getOrDefault(grid.find(linearAddress(row, col)), 0);
+        return state == 1 || state == 3;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return percolates;
+//        for (int j = 0; j < N; j++) {
+//            if (isOpen(N - 1, j)) {
+//                return (boolean) sites.getOrDefault(grid.find(linearAddress(N-1, j)), false);
+//                }
+//            }
+//        return false;
+    }
+
+    public static void main(String[] args) {
+        Percolation p = new Percolation(10);
+        p.open(0, 4);
+        p.open(1, 4);
+
+    }
 
 }
--- Homeworks/hw2/hw2/PercolationStats.java
+++ Homeworks/hw2/hw2/PercolationStats.java
@@ -1,5 +1,63 @@
+// https://fa20.datastructur.es/materials/hw/hw2/hw2
+
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+import java.util.Arrays;
+
+public class PercolationStats {
+    private double[] thresholds;
+    private int T;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.T = T;
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Both N and T have to be positive.");
+        }
+        thresholds = new double[T];
+        for(int i = 0; i < T; i++) {
+            percolationExperiment(i, N, thresholds, pf);
+        }
+    }
+
+    private void percolationExperiment(int i, int N, double[] thresholds, PercolationFactory pf) {
+        Percolation surface = pf.make(N);
+        while (!surface.percolates()) {
+            int row, col;
+            do {
+                row = StdRandom.uniform(0, N);
+                col = StdRandom.uniform(0, N);
+            } while (surface.isOpen(StdRandom.uniform(0, N), StdRandom.uniform(0, N)));
+            surface.open(row, col);
+        }
+        thresholds[i] = (double) (surface.numberOfOpenSites()) / (N * N);
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public static void main(String[] args) {
+        PercolationStats ps = new PercolationStats(20, 1000, new PercolationFactory());
+        System.out.println(ps.mean());
+    }
 }
